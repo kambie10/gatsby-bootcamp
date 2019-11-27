31a0d9f9c006f9c236ec4e4004a1d44632263e87
David Maulana <davidmaulana10k@gmail.com>
2019-11-27 11:32:55 +0700

    set style header

--- src/components/header.js
+++ src/components/header.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import { Link }from 'gatsby'
 
+import styles from './header.module.scss'
+
 const Header = () => {
   return (
     <header>
@@ -8,7 +10,7 @@ const Header = () => {
       <nav>
         <ul>
           <li>
-            <Link to="/">Home</Link>
+            <Link className={styles.link} to="/">Home</Link>
           </li>
           <li>
             <Link to="/about">About</Link>
