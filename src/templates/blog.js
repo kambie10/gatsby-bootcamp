7fa2f64dfedd5a597a38e7a1dd302b66d0f1ef2d
David Maulana <davidmaulana10k@gmail.com>
2019-12-13 19:12:56 +0700

    link blog list

---
+++ src/templates/blog.js
@@ -0,0 +1,13 @@
+import React from "react"
+
+import Layout from "../components/layout"
+
+const Blog = () => {
+  return (
+    <Layout >
+      this is blog tamplate
+    </Layout>
+  )
+}
+
+export default Blog
