b7728e5d988c45fdb98f652b66915e212ae72d16
David Maulana <davidmaulana10k@gmail.com>
2019-12-16 00:39:17 +0700

    Link blog

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -27,12 +27,13 @@ const BlogPage = () => {
       <ol>
         {data.allMarkdownRemark.edges.map((edge) => {
           return (
-            <Link to={`/blog/${edge.node.fields.slug}`}>
               <li>
+                <Link to={`/blog/${edge.node.fields.slug}`}>
                   <h2>{edge.node.frontmatter.title}</h2>
                   <p>{edge.node.frontmatter.date}</p>
+                </Link>
+
               </li>
-            </Link>
 
           )
         })}
